39985b1a99b57ee2b52296e435de5d5fb5847120
Antonio Pezzino <antopez1984@gmail.com>
2024-05-28 21:08:36 +0200

    aggiunta

--- php_01_Antonio_Pezzino/traccia3.php
+++ php_01_Antonio_Pezzino/traccia3.php
@@ -98,7 +98,16 @@ $words2 = [
                   ] 
 ];
 
+/*
 $results =  $words1[6][3][1][1] . " " . $words1[6][3][0] . " " . $words1[8] . " " . $words1[6][3][1][0] . " " . $words1[8] . " " . $words2['elemento2']  . " " .
+            $words1[2] . " " . $words1[4] . " " . $words1[6][4] . " " . $words1[6][5] . " " . $words1[6][3][1][2][2][1] . " " . 
+            $words1[6][3][1][2][0] . " " . $words1[6][0] . ", ". $words2['elemento3'][2] . " " . $words1[6][3][1][2][1] . " " . $words1[7] . " " . $words1[6][3][1][2][2][0] . " " . 
+            $words1[6][1] . " " . $words2['elemento3'][1];
+
+echo ($results); */
+
+
+$results =  $words1[6][3][1][1] . " di " . $words1[6][3][0] . " di " . $words1[8] . " " . $words1[6][3][1][0] . " " . $words1[8] . " " . $words2['elemento2']  . " " .
             $words1[2] . " " . $words1[4] . " " . $words1[6][4] . " " . $words1[6][5] . " " . $words1[6][3][1][2][2][1] . " " . 
             $words1[6][3][1][2][0] . " " . $words1[6][0] . ", ". $words2['elemento3'][2] . " " . $words1[6][3][1][2][1] . " " . $words1[7] . " " . $words1[6][3][1][2][2][0] . " " . 
             $words1[6][1] . " " . $words2['elemento3'][1];
